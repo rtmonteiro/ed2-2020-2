61020d5f7d7e6a2f42af6a40f1a2418b5b388bbd
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2021-02-13 19:34:17 -0300

    mudando o tipo do vetor para char

--- roteiro1/crivo_de_eratostenes/main.c
+++ roteiro1/crivo_de_eratostenes/main.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 #include <time.h>
 
-// #define N 4194304 // char
+#define N 8000000 // char 8.388.608
 // #define N 2097152 // int
-#define N 2000000 // int
 // #define N 1000
 
 int main() {
     int i, j;
-    int a[N+1];
-    for (a[1] = 0, i = 2 ; i <= N; i++) a[i] = 1;
+    char a[N+1];
+    for (a[1] = '0', i = 2 ; i <= N; i++) a[i] = '1';
 
     clock_t start = clock();
     for (i = 2; i <= N/2; i++) {
-        if(a[i] == 1) {
+        if(a[i] == '1') {
             for (j = 2; j <= N/i; j++) {
-                a[i*j] = 0;
+                a[i*j] = '0';
             }
         }
     }
     clock_t end = clock();
 
-    for (int i = 1; i <= N; ++i) {
-        if (a[i] == 1) printf("%d ", i);
-    }
+//    for (int i = 1; i <= N; ++i) {
+//        if (a[i] == '1') printf("%d ", i);
+//    }
 
-    printf("\n");
-    double seconds = (( double ) end - start ) / CLOCKS_PER_SEC ;
-    printf (" tempo em segundos: % lf \n" , seconds );
+    double seconds = (( float ) end - start ) / CLOCKS_PER_SEC ;
+    printf ("\n\ttempo em segundos: % f \n" , seconds );
 
 
     return 0;

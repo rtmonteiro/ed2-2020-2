2d79ce8d32a9140df4b4066098a4e5a220d945f4
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2021-02-15 15:52:45 -0300

    criei a API do crivo

---
+++ roteiro1/crivo_de_eratostenes/crivo.h
@@ -0,0 +1,14 @@
+//
+// Created by Pichau on 15/02/2021.
+//
+
+#ifndef CRIVO_DE_ERATOSTENES_CRIVO_H
+#define CRIVO_DE_ERATOSTENES_CRIVO_H
+
+void* initArray(int magnitude);
+
+void findPrimes(void *array, int magnitude);
+
+void printPrimes(void *array, int magnitude);
+
+#endif //CRIVO_DE_ERATOSTENES_CRIVO_H

31ba387a79cb63808054d07f1d4d4d7a2dc58b5a
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2021-03-03 08:37:57 -0300

    sort: inicia o relatorio de sort

---
+++ roteiro3/sort/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "item.h"
+
+extern void sort(Item *a, int lo, int hi);
+
+int main(int argc, char *argv[]) {
+
+    int N = (int) strtol(argv[1], NULL, 10);
+    char *FILENAME = argv[2];
+
+    Item *arr = (Item *) malloc(sizeof(Item) * N);
+
+    FILE *fp = fopen(FILENAME, "r");
+    if (!fp) {
+        fprintf(stderr, "Error opening file '%s'\n", FILENAME);
+        return EXIT_FAILURE;
+    }
+
+    char *line_buf = NULL;
+    size_t line_buf_size = 0;
+
+    for (int i = 0; i < N; ++i) {
+        getline(&line_buf, &line_buf_size, fp);
+        arr[i] = (Item) strtol(line_buf, NULL, 10);
+        printf("%d ", arr[i]);
+    }
+
+    sort(arr, 0, N);
+    printf("\n");
+
+    for (int i = 0; i < N; ++i) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("IUPI!\n");
+    return 0;
+}

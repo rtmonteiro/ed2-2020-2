dab45962ec1e30c134a25a820bf2267f1e25ecb4
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2021-03-22 11:51:26 -0300

    merge: implementa mergesort com cutoff
    mergesort top-down recursivo com cutoff de 5 itens

--- aula11-mergesort/mergesort.c
+++ aula11-mergesort/mergesort.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include "item.h"
 
+#define CUTOFF 5
+
 static void merge(Item *a, Item *aux, int lo, int mid, int hi) {
     for (int k = lo; k <= hi; ++k) aux[k] = a[k];
     int i = lo, j = mid+1;
@@ -16,8 +18,23 @@ static void merge(Item *a, Item *aux, int lo, int mid, int hi) {
     }
 }
 
+void insert_sort(Item *a, int lo, int hi){
+    for (int i = lo; i <= hi; ++i) {
+        int j = i - 1;
+        int k = a[i];
+        while (j>=lo && less(k, a[j])){
+            a[j+1] = a[j];
+            --j;
+        }
+        a[j+1] = k;
+    }
+}
+
 static void merge_sort(Item *a, Item *aux, int lo, int hi) {
-    if (hi <= lo) return;
+    if (hi <= lo + CUTOFF - 1) {
+        insert_sort(a, lo, hi);
+        return;
+    }
     int mid = lo + (hi - lo) / 2;
     merge_sort(a, aux, lo, mid);
     merge_sort(a, aux, mid+1, hi);

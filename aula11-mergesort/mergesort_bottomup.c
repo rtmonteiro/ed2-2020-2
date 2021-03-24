999207efb6df183bdbbb2180f6bddac4ce971566
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2021-03-24 11:20:50 -0300

    merge: mergesort bottom-up com cutoff
    mergesort bottom-up com cutoff de 10

--- aula11-mergesort/mergesort_bottomup.c
+++ aula11-mergesort/mergesort_bottomup.c
@@ -2,11 +2,28 @@
 // Created by monte on 23/03/2021.
 //
 
-#include <stdio.h>
 #include <stdlib.h>
 #include "item.h"
 
+#define CUTOFF 10
+
+void insert_sort(Item *a, int lo, int hi){
+    for (int i = lo + 1; i < hi; ++i) {
+        int j = i - 1;
+        int k = a[i];
+        while (j>=lo && less(k, a[j])){
+            a[j+1] = a[j];
+            --j;
+        }
+        a[j+1] = k;
+    }
+}
+
 static void merge(Item *a, Item *aux, int lo, int mid, int hi) {
+    if (hi <= lo + CUTOFF - 1) {
+        insert_sort(a, lo, hi);
+        return;
+    }
     for (int k = lo; k < hi; ++k) aux[k] = a[k];
     int i = lo, j = mid;
     for (int k = lo; k < hi; ++k) {

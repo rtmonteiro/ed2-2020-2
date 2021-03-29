7a7280b794f2f7dd4876ce23bd8eb88079c9bd46
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2021-03-29 10:17:09 -0300

    quick: implementa quicksort sem o shuffle

--- aula13-quicksort/quicksort.c
+++ aula13-quicksort/quicksort.c
@@ -2,8 +2,6 @@
 // Created by Pichau on 28/03/2021.
 //
 
-#include <stdlib.h>
-#include <sys/time.h>
 #include "item.h"
 
 int partition(Item *a, int lo, int hi) {
@@ -13,19 +11,10 @@ int partition(Item *a, int lo, int hi) {
         while (less(a[++i], v)) if (i == hi) break;
         while (less(v, a[--j])) if (j == lo) break;
         if (i >= j) break;
-        exch(a[i], a[j]);
+        exch(a[i], a[j])
     }
-    exch(a[lo], a[j]);
+    exch(a[lo], a[j])
     return j;
-}
-
-void shuffle(Item *a, int N) {
-    struct timeval tv; gettimeofday(&tv, NULL);
-    srand48(tv.tv_usec);
-    for (int i = N-1; i > 0; i--) {
-        unsigned int j = (unsigned int) (drand48()*(i+1));
-        exch(a[i], a[j]);
-    }
 }
 
 static void quick_sort(Item *a, int lo, int hi) {
@@ -36,6 +25,5 @@ static void quick_sort(Item *a, int lo, int hi) {
 }
 
 void sort(Item *a, int lo, int hi) {
-    shuffle(a, hi - lo + 1);
     quick_sort(a, lo, hi);
 }

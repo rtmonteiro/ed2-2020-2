df4291c92bf155df70fed4b061caa9fc9e424fb9
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2021-03-29 10:42:40 -0300

    quick: implementa quicksort com cutoff de 2

--- aula13-quicksort/quicksort.c
+++ aula13-quicksort/quicksort.c
@@ -4,6 +4,20 @@
 
 #include "item.h"
 
+#define CUTOFF 2
+
+void insert_sort(Item *a, int lo, int hi){
+    for (int i = lo + 1; i <= hi; ++i) {
+        int j = i - 1;
+        int k = a[i];
+        while (j>=lo && less(k, a[j])){
+            a[j+1] = a[j];
+            --j;
+        }
+        a[j+1] = k;
+    }
+}
+
 int partition(Item *a, int lo, int hi) {
     int i = lo, j = hi+1;
     Item v = a[lo];
@@ -18,7 +32,10 @@ int partition(Item *a, int lo, int hi) {
 }
 
 static void quick_sort(Item *a, int lo, int hi) {
-    if (hi <= lo) return;
+    if (hi <= lo + CUTOFF - 1) {
+        insert_sort(a, lo, hi);
+        return;
+    }
     int j = partition(a, lo, hi);
     quick_sort(a, lo, j-1);
     quick_sort(a, j+1, hi);
